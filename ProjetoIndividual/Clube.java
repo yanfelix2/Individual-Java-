e39404845218f9e52380cc58b7ded12ee6299dda
yanfelix2 <yanfelixpimentel@hotmail.com>
2024-09-24 12:29:41 -0300

    projeto individual

---
+++ ProjetoIndividual/Clube.java
@@ -0,0 +1,23 @@
+package ProjetoIndividual;
+
+
+public class Clube {
+    public String nomeClube;
+    public String dataFundacao;
+    public String nomePresidente;
+    public String jogadores;
+    public String siteOficial;
+    public String numeroContato;
+    public String historicoPartidas;
+    public String localizacaoSede;
+    public String Estadio;
+
+    
+
+
+
+    
+
+
+    
+}
---
+++ ProjetoIndividual/Jogador.java
@@ -0,0 +1,19 @@
+package ProjetoIndividual;
+
+public class Jogador {
+    public String nome;
+    public String nacionalidade;
+    public int numeroCamisa;
+    public String posicao;
+    public String dataNascimento;
+    public String estatisticas;
+    public Double altura;
+    public Double peso;
+    public String clubeAtual;
+    public String historicoClubes;
+    public Double salario;
+
+
+    
+    
+}
