7e358736bd23b5b189bbf492f66b131f803ba06b
yanfelix2 <yanfelixpimentel@hotmail.com>
2024-10-10 14:16:48 -0300

    Atualização get e set

--- ProjetoIndividual/Clube.java
+++ ProjetoIndividual/Clube.java
@@ -2,22 +2,75 @@ package ProjetoIndividual;
 
 
 public class Clube {
-    public String nomeClube;
-    public String dataFundacao;
+    public String nome;
     public String nomePresidente;
-    public String jogadores;
-    public String siteOficial;
     public String numeroContato;
-    public String historicoPartidas;
-    public String localizacaoSede;
-    public String Estadio;
-    public String teste;
+    public String estadio;  
+
+    public Clube() {
+    }
+
+    public Clube(String nome, String nomePresidente, String numeroContato, String estadio){
+        this.nome = nome;
+        this.nomePresidente = nomePresidente;
+        this.numeroContato = numeroContato;
+        this.estadio = estadio;
+    }
+
+        //GET E SET ---Nome Do Clube--- // 
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+
+        //GET E SET ---NomeDoPresidente---// 
+
+
+    public String getNomePresidente() {
+        return nomePresidente;
+    }
+
+    public void setNomePresidente(String nomePresidente){
+        this.nomePresidente = nomePresidente;
+    }
+
+        //GET E SET ---Numero Contato---//
+
+    
+    public String getNumeroContato() {
+        return numeroContato;
+    }
+
+    public void setNumeroContato(String numeroContato){
+        this.numeroContato = numeroContato;
+    }
+
+        //GET E SET ---Estadio---//
+
+
+    public String getEstadio() {
+        return estadio;
+    }
+
+    public void setEstadio(String estadio) {
+        this.estadio = estadio;
+    }
+
+
+    
 
     
 
 
 
-    
+
+
+
 
 
     
