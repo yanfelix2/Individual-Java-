de7bfefaaa796b4b830bc7d12d6a07cce239c31b
yanfelix2 <yanfelixpimentel@hotmail.com>
2024-10-10 11:17:14 -0300

    att aula 09

---
+++ Membro.java
@@ -0,0 +1,17 @@
+package ProjetoIndividual;
+
+public class Membro {
+    public String nome;
+    public String cargo;
+    public String dataContratacao;
+    public int tempoContrato;
+    public Double salario;
+    public String emailContato;
+    public String telefoneContato;
+    public String cpf;
+    public String dataNascimento;
+    public String departamento;
+    public String formacao;
+    public String historicoClubes;
+    
+}
---
+++ Patrocinador.java
@@ -0,0 +1,23 @@
+package ProjetoIndividual;
+
+public class Patrocinador {
+    public String nomeEmpresa;
+    public String cnpj;
+    public String tipoPatrocinio;
+    public Double valorPatrocinio;
+    public String dataInicioContrato;
+    public String dataFimContrato;
+    public String statusContrato;
+    public String emailContato;
+    public String telefoneContato;
+    public String logoEmpresa;
+    public String clubePatrocinado;
+    
+   
+
+    
+
+
+
+    
+}
