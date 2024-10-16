2773721fb7fdb07d242af0a9bd69cffe15e016b9
yanfelix2 <yanfelixpimentel@hotmail.com>
2024-10-16 15:22:28 -0300

    Atualização

--- ProjetoIndividual/Jogador.java
+++ ProjetoIndividual/Jogador.java
@@ -3,16 +3,17 @@ package ProjetoIndividual;
 public class Jogador {
     public String nome;
     public String nacionalidade;
-    public int numeroCamisa;
-    public String posicao;
-    public String dataNascimento;
-    public String estatisticas;
+    public Double salario;
     public Double altura;
     public Double peso;
+    public int numeroCamisa;
+    public String posicao;
     public String clubeAtual;
-    public String historicoClubes;
-    public Double salario;
 
+    
+    
+
+ 
 
     
     
